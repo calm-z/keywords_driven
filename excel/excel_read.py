f59d1fcb0ec8a22efebdb11eb850b79b40c5b274
calm-z <calmz@foxmail.com>
2020-07-02 18:02:53 +0800

    log is done!

--- excel/excel_read.py
+++ excel/excel_read.py
@@ -3,8 +3,17 @@
 # 日期：2020/6/30 17:31
 # 工具：PyCharm
 # Python版本：3.7
+# https://www.imooc.com/article/302606
+from openpyxl import Workbook
 
-import openpyxl
+wb = Workbook()
+ws = wb.active
+ws1 = wb.create_sheet('Mysheet')
+ws2 = wb.create_sheet('Mysheet', 0)
+ws3 = wb.create_sheet('Mysheet', -1)
+ws.title = "New Title"
+print(wb.sheetnames)
+for sheet in wb:
+    print(sheet)
 
-wk = openpyxl.workbook()
 
